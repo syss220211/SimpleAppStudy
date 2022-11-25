2f630781e813f0c7c8e89c01dcb2adbfd550af20
syss220211 <syss220211@gmail.com>
2022-11-25 16:13:24 +0900

    Initial Commit

---
+++ SimpleAppStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2022/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
