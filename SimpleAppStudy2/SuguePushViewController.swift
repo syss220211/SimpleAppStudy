fe8bdd25167ec05a248398bb9e78a932996526c7
syss220211 <syss220211@gmail.com>
2022-11-30 18:55:04 +0900

    segue push fin, back button

---
+++ SimpleAppStudy2/SuguePushViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SuguePushViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2022/11/30.
+//
+
+import UIKit
+
+class SuguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
