8cb8f4d6a65aecdefde8a211b0a7da94df29485c
syss220211 <syss220211@gmail.com>
2022-12-09 18:54:22 +0900

    code push not

--- SimpleAppStudy2/CodePushViewController.swift
+++ SimpleAppStudy2/CodePushViewController.swift
@@ -15,10 +15,8 @@ class CodePushViewController: UIViewController {
     }
     
     @IBAction func codePushButton(_ sender: UIButton) {
-        // 스토리보드 뷰컨 인스턴스화
-        // 액션 함수 안에 네비 스택의 코드 푸시 뷰컨이 푸시 되도록 작성
-//        guard let viewController = self.storyboard?.instantiateViewController(Identifier: "CodePushViewController")
-//        else { return }
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") else { return }
+        self.navigationController?.pushViewController(viewController, animated: true)
+        
     }
-    
 }
