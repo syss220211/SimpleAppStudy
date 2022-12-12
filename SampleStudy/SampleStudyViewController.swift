25af299610a0f162c0ad0f6fb4b6efe767e6fa0e
syss220211 <syss220211@gmail.com>
2022-12-12 23:50:03 +0900

    navi study alone

---
+++ SampleStudy/SampleStudyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SampleStudyViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2022/12/12.
+//
+
+import UIKit
+
+class SampleStudyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
