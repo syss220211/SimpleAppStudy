bf8ded50568ba1f787df9d1b319b5283b91bbdba
syss220211 <syss220211@gmail.com>
2022-12-15 02:28:41 +0900

    fix button

--- SampleStudy/SeguePushViewController.swift
+++ SampleStudy/SeguePushViewController.swift
@@ -11,7 +11,6 @@ class SeguePushViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
     }
 
---
+++ SampleStudy/presentSegueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  presentSegueViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2022/12/14.
+//
+
+import UIKit
+
+class presentSegueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
