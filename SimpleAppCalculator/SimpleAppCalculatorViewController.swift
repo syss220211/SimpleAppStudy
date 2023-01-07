e309d40d68b78fc03d28089205a6d4b72a42c145
syss220211 <syss220211@gmail.com>
2023-01-07 23:50:37 +0900

    Calculator start

---
+++ SimpleAppCalculator/SimpleAppCalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SimpleAppCalculatorViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2023/01/07.
+//
+
+import UIKit
+
+class SimpleAppCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
