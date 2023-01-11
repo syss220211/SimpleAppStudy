3f3442868ab11c4f12be9ab030982fc9aae3f98c
syss220211 <syss220211@gmail.com>
2023-01-11 15:25:02 +0900

    Todo List App start

---
+++ SimpleAppTodo/SimpleAppTodoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleAppTodoViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2023/01/11.
+//
+
+import UIKit
+
+class SimpleAppTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
