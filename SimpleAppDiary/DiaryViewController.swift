0d7d305399ac522ee6fb1fdc0cf8e7541d57dd73
syss220211 <syss220211@gmail.com>
2023-01-18 23:50:54 +0900

    diary app start

---
+++ SimpleAppDiary/DiaryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2023/01/18.
+//
+
+import UIKit
+
+class DiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
