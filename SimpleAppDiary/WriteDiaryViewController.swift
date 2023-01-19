5be02e336e94ff046df7bd19fca41c8c7935d48c
syss220211 <syss220211@gmail.com>
2023-01-19 16:57:19 +0900

    CH05-05 fin

---
+++ SimpleAppDiary/WriteDiaryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WriteDiaryViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2023/01/19.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
