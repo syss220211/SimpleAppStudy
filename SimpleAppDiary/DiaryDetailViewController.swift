4364058a7ecfa4c6e2e9253388e08c0feeaaf102
syss220211 <syss220211@gmail.com>
2023-01-20 13:41:29 +0900

    diary UI 그리기 완료

---
+++ SimpleAppDiary/DiaryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryDetailViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2023/01/20.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
