b450757b79cec83b2e9ec9e6ece8c3b3af37451b
syss220211 <syss220211@gmail.com>
2023-02-01 01:01:58 +0900

    즐겨찾기 탭에도 즐겨찾기 된 cell 표시하기

--- SimpleAppDiary/StarCell.swift
+++ SimpleAppDiary/StarCell.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class StarCell: UICollectionViewCell {
     
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    // 테두리 추가
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.contentView.layer.cornerRadius = 3.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+    }
 }
