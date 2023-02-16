fabd61e44ccf2c7af1147a9cbee3181b02cf03d8
syss220211 <syss220211@gmail.com>
2023-02-16 18:45:27 +0900

    outlet 변수 연결

--- SimplePomodoro/PomodoroViewController.swift
+++ SimplePomodoro/PomodoroViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class PomodoroViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var progreeView: UIProgressView!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var toggleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
