2d6db71725e96f23198118553cbf3f8e724f2cee
syss220211 <syss220211@gmail.com>
2023-02-23 23:33:12 +0900

    계산기 앱 주석 추가

--- SimpleAppCalculator/RoundButton.swift
+++ SimpleAppCalculator/RoundButton.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 // UIButton Custom
+// UIButton 을 상속 받음 = 기존 버튼 속성 + 커스텀 속성이 사용 가능한 클래스
 
 @IBDesignable // 변경된 속성 실시간으로 스토리 보드에서 확인가능 (
 class RoundButton: UIButton {
@@ -15,7 +16,7 @@ class RoundButton: UIButton {
     @IBInspectable var isRound: Bool = false { // 스토리보드에서도 프로퍼티를 사용할 수 있도록 @
         didSet {
             if isRound { // isRound = Ture
-                self.layer.cornerRadius = self.frame.height / 2
+                self.layer.cornerRadius = self.frame.height / 2 // 둥글게  
             }
         }
     }
