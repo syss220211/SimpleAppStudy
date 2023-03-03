114e66ef61935f99a887b1937502ab664343be77
syss220211 <syss220211@gmail.com>
2023-03-03 15:33:09 +0900

    날씨앱 설정

---
+++ SimpleWeather/weatherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  weatherViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2023/03/03.
+//
+
+import UIKit
+
+class weatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
