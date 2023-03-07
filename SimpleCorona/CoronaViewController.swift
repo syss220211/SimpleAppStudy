29fb31f7bb73d50f31bb93da6615f70c9c5edb92
syss220211 <syss220211@gmail.com>
2023-03-07 23:57:48 +0900

    api 구현 - 1

--- SimpleCorona/CoronaViewController.swift
+++ SimpleCorona/CoronaViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Charts
+import Alamofire
 
 class CoronaViewController: UIViewController {
 
@@ -16,7 +17,43 @@ class CoronaViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fetchCovidOverview { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                debugPrint("success \(result)")
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+        }
 
+    }
+    
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ) {
+        let url = "http://api.cornoa-19.kr/korea/county/new"
+        let param = [
+            "serviceKey" : "4Cq5B9KNYdF2msP31baTgGRSJAOVrfnhu"
+        ]
+        
+        AF.request(url, method: .get, parameters: param) // api 호출
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                    // 요청 실패시
+                case let .failure(error):
+                    completionHandler(.failure(error))
+
+                }
+            })
     }
     
 
