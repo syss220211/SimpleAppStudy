adc831a7460500c6ca9f516754f7a856c7fd094d
syss220211 <syss220211@gmail.com>
2023-04-07 23:46:01 +0900

    tableView

---
+++ DdayApp/SettingTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingTableViewController.swift
+//  SimpleAppStudy
+//
+//  Created by 박서연 on 2023/04/07.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+
+}
